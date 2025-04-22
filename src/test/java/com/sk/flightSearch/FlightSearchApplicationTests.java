839a5109377802da93073b7bd1459f2d4caa0f9e
BrahiamAcosta <brahiam.acosta@udea.edu.co>
2025-04-22 02:43:22 -0500

    Initial commit

---
+++ src/test/java/com/sk/flightSearch/FlightSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.flightSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
